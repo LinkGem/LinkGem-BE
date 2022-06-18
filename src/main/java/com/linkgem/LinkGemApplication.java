0a21a51de190738c5d7e57cee3f26ea97b13f8fe
bamdul <Bamdule5@herren.co.kr>
2022-06-18 20:46:01 +0900

    feat(LG-21): 초기 설정 파일을 생성한다

---
+++ src/main/java/com/linkgem/LinkGemApplication.java
@@ -0,0 +1,13 @@
+package com.linkgem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkGemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinkGemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/linkgem/LinkGemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linkgem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkGemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
