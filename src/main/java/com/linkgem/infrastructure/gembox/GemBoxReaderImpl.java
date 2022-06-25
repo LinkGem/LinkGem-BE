4f0885ae06814e87100eec43626fdef2200edbf2
bamdul <Bamdule5@herren.co.kr>
2022-06-25 16:26:15 +0900

    feat(LG-39): 잼박스 Reader, Store 인터페이스를 구현한다

---
+++ src/main/java/com/linkgem/infrastructure/gembox/GemBoxReaderImpl.java
@@ -0,0 +1,33 @@
+package com.linkgem.infrastructure.gembox;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Repository;
+
+import com.linkgem.domain.gembox.GemBox;
+import com.linkgem.domain.gembox.GemBoxReader;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Repository
+public class GemBoxReaderImpl implements GemBoxReader {
+
+    private final GemBoxRepository repository;
+
+    @Override
+    public Optional<GemBox> find(String name, Long userId) {
+        return repository.findByNameAndUserId(name, userId);
+    }
+
+    @Override
+    public Optional<GemBox> find(Long id) {
+        return repository.findById(id);
+    }
+
+    @Override
+    public List<GemBox> findAll(Long userId) {
+        return repository.findAllByUserId(userId);
+    }
+}
