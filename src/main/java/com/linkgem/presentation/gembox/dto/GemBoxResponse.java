615a084adb2fb7e3d8d66e16a6cdf802999f4250
bamdul <Bamdule5@herren.co.kr>
2022-06-26 18:07:59 +0900

    feat(LG-41): 잼박스 요청/응답 dto 클래스를 생성한다

---
+++ src/main/java/com/linkgem/presentation/gembox/dto/GemBoxResponse.java
@@ -0,0 +1,35 @@
+package com.linkgem.presentation.gembox.dto;
+
+import com.linkgem.domain.gembox.GemBoxInfo;
+
+import io.swagger.annotations.ApiModel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+public class GemBoxResponse {
+
+    @ApiModel(description = "잼박스 목록 응답")
+    @AllArgsConstructor
+    @Getter
+    public static class GemBox {
+        private Long id;
+        private String name;
+
+        public static GemBox of(GemBoxInfo.Main create) {
+            return new GemBox(create.getId(), create.getName());
+        }
+    }
+
+    @ApiModel(description = "잼박스 생성 응답")
+    @AllArgsConstructor
+    @Getter
+    public static class CreateResponse {
+        private Long id;
+        private String name;
+
+        public static CreateResponse of(GemBoxInfo.Create create) {
+            return new CreateResponse(create.getId(), create.getName());
+        }
+    }
+
+}
