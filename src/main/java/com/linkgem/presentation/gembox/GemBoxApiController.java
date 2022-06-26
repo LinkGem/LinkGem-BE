88b5743677914accaeb824bd1a167471bd3dde78
bamdul <Bamdule5@herren.co.kr>
2022-06-26 22:45:21 +0900

    feat(LG-42): 잼박스 삭제, 상세조회 API를 작성한다

--- src/main/java/com/linkgem/presentation/gembox/GemBoxApiController.java
+++ src/main/java/com/linkgem/presentation/gembox/GemBoxApiController.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import javax.validation.Valid;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,19 +18,49 @@ import org.springframework.web.bind.annotation.RestController;
 import com.linkgem.application.GemBoxFacade;
 import com.linkgem.domain.gembox.GemBoxCommand;
 import com.linkgem.domain.gembox.GemBoxInfo;
+import com.linkgem.domain.gembox.GemBoxQuery;
 import com.linkgem.presentation.common.CommonResponse;
 import com.linkgem.presentation.gembox.dto.GemBoxRequest;
 import com.linkgem.presentation.gembox.dto.GemBoxResponse;
 
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 
+@Api(tags = "잼박스")
 @RequiredArgsConstructor
 @RequestMapping(value = "/api/gembox")
 @RestController
 public class GemBoxApiController {
 
     private final GemBoxFacade gemBoxFacade;
+
+    @ApiOperation(value = "잼박스 조회", notes = "잼박스를 조회한다")
+    @GetMapping(value = "/{id}")
+    public CommonResponse<GemBoxResponse.GemBox> find(
+        @ApiParam(value = "잼박스 고유 아이디", example = "1") @PathVariable Long id
+    ) {
+
+        // :TODO User Id를 토큰에서 추출하는 방법을 결정해야한다.
+        Long temporaryUserId = 1L;
+        GemBoxInfo.Main gemboxInfo = gemBoxFacade.find(GemBoxQuery.SearchDetail.of(id, temporaryUserId));
+
+        return CommonResponse.of(GemBoxResponse.GemBox.of(gemboxInfo));
+    }
+
+    @ApiOperation(value = "잼박스 목록 조회", notes = "잼박스 목록을 조회한다")
+    @GetMapping
+    public CommonResponse<List<GemBoxResponse.GemBox>> findAll() {
+
+        // :TODO User Id를 토큰에서 추출하는 방법을 결정해야한다.
+        Long temporaryUserId = 1L;
+        List<GemBoxResponse.GemBox> responses = gemBoxFacade.findAll(temporaryUserId)
+            .stream().map(GemBoxResponse.GemBox::of)
+            .collect(Collectors.toList());
+
+        return CommonResponse.of(responses);
+    }
 
     @ApiOperation(value = "잼박스 생성", notes = "잼박스를 생성한다")
     @PostMapping
@@ -47,7 +78,7 @@ public class GemBoxApiController {
     @ApiOperation(value = "잼박스 수정", notes = "잼박스를 수정한다")
     @PatchMapping(value = "/{id}")
     public ResponseEntity<Void> update(
-        @PathVariable Long id,
+        @ApiParam(value = "잼박스 고유 아이디", example = "1") @PathVariable Long id,
         @RequestBody @Valid GemBoxRequest.UpdateRequest request) {
 
         // :TODO User Id를 토큰에서 추출하는 방법을 결정해야한다.
@@ -58,18 +89,16 @@ public class GemBoxApiController {
         return ResponseEntity.noContent().build();
     }
 
-    @ApiOperation(value = "잼박스 목록 조회", notes = "잼박스 목록을 조회한다")
-    @GetMapping
-    public CommonResponse<List<GemBoxResponse.GemBox>> findAll(
+    @ApiOperation(value = "잼박스 삭제", notes = "잼박스를 삭제한다")
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(
+        @ApiParam(value = "잼박스 고유 아이디", example = "1") @PathVariable Long id
     ) {
 
         // :TODO User Id를 토큰에서 추출하는 방법을 결정해야한다.
         Long temporaryUserId = 1L;
-        List<GemBoxResponse.GemBox> responses = gemBoxFacade.findAll(temporaryUserId)
-            .stream().map(GemBoxResponse.GemBox::of)
-            .collect(Collectors.toList());
+        gemBoxFacade.delete(GemBoxCommand.Delete.of(id, temporaryUserId));
 
-        return CommonResponse.of(responses);
+        return ResponseEntity.noContent().build();
     }
-
 }
