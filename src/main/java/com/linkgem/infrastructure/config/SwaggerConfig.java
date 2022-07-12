800fc71628ad0bbed396b3cbcbb077a735b26a01
bamdule <bamdule5@gmail.com>
2022-07-13 00:00:22 +0900

    feat: 스웨거에서 access_token을 입력할 수 있도록 수정한다

--- src/main/java/com/linkgem/infrastructure/config/SwaggerConfig.java
+++ src/main/java/com/linkgem/infrastructure/config/SwaggerConfig.java
@@ -4,6 +4,7 @@ package com.linkgem.infrastructure.config;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -14,6 +15,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 
 import com.fasterxml.classmate.TypeResolver;
+import com.google.common.collect.Lists;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
@@ -24,7 +26,11 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.schema.AlternateTypeRules;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -45,10 +51,12 @@ public class SwaggerConfig {
             .apiInfo(apiInfo())
             .groupName("API V1")
             .select()
-            .paths(PathSelectors.regex("/api.*"))
+            .paths(PathSelectors.regex("/api/v1.*"))
             .build()
             .consumes(getConsumes())
             .produces(Stream.of(MediaType.APPLICATION_JSON_VALUE).collect(Collectors.toSet()))
+            .securitySchemes(Lists.newArrayList(apiKey()))
+            .securityContexts(Lists.newArrayList(securityContext()))
             .useDefaultResponseMessages(false)
             .directModelSubstitute(LocalDateTime.class, String.class)
             .directModelSubstitute(LocalDate.class, String.class)
@@ -69,6 +77,24 @@ public class SwaggerConfig {
                 MediaType.APPLICATION_FORM_URLENCODED_VALUE,
                 MediaType.MULTIPART_FORM_DATA_VALUE)
             .collect(Collectors.toSet());
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("accessToken", "ACCESS_TOKEN", "header");
+    }
+
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Lists.newArrayList(new SecurityReference("accessToken", authorizationScopes));
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+            .securityReferences(defaultAuth())
+            .forPaths(PathSelectors.regex("/api/v1.*"))
+            .build();
     }
 
     @Getter
