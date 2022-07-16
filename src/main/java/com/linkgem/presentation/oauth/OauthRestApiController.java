57bb985276597ead67f768ae1c9427d7cef64904
HOSE JOO <hose0728@naver.com>
2022-07-16 17:03:00 +0900

    fix(login logic): fix login response add 200 code

--- src/main/java/com/linkgem/presentation/oauth/OauthRestApiController.java
+++ src/main/java/com/linkgem/presentation/oauth/OauthRestApiController.java
@@ -8,6 +8,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,21 +27,21 @@ public class OauthRestApiController {
 
   @ApiOperation(value = "소셜로그인", notes = "소셜 로그인을 진행한다.")
   @GetMapping("/login/{provider}")
-  public CommonResponse<LoginResponse> login(
+  public ResponseEntity<CommonResponse<LoginResponse>> login(
       @ApiParam(value = "provider", example = "naver") @PathVariable String provider,
       @ApiParam(value = "인증코드") @RequestParam String code) {
     LoginResponse loginResponse = oauthService.login(provider, code);
-    return CommonResponse.of(loginResponse);
+    return ResponseEntity.ok(CommonResponse.of(loginResponse));
   }
 
   @ApiOperation(value = "Access-Token 재발급", notes = "Access-Token을 재발급한다.")
   @PostMapping("/reissue")
-  public CommonResponse<TokenReissueResponse> reissue(
+  public ResponseEntity<CommonResponse<TokenReissueResponse>> reissue(
       @RequestHeader(value = "ACCESS-TOKEN") String accessToken,
       @RequestHeader(value = "REFRESH-TOKEN") String refreshToken) {
     TokenReissueResponse tokenReissueResponse = oauthService.reissue(accessToken, refreshToken);
 
-    return CommonResponse.of(tokenReissueResponse);
+    return ResponseEntity.ok((CommonResponse.of(tokenReissueResponse)));
   }
 
 
