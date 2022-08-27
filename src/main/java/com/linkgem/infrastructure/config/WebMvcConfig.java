8b0f2c4667b1d2b6b07ee20504a166036c77b6c7
bamdule <bamdule5@gmail.com>
2022-08-27 15:39:02 +0900

    feat: CorsFilter 컴포넌트를 추가한다

--- src/main/java/com/linkgem/infrastructure/config/WebMvcConfig.java
+++ src/main/java/com/linkgem/infrastructure/config/WebMvcConfig.java
@@ -1,39 +1,27 @@
 package com.linkgem.infrastructure.config;
 
-import com.linkgem.infrastructure.config.Interceptor.JwtTokenInterceptor;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@EnableWebMvc
+import com.linkgem.infrastructure.config.Interceptor.JwtTokenInterceptor;
+
+import lombok.RequiredArgsConstructor;
+
 @Configuration
 @RequiredArgsConstructor
 public class WebMvcConfig implements WebMvcConfigurer {
 
-  private final JwtTokenInterceptor jwtTokenInterceptor;
-  @Override
-  public void addInterceptors(InterceptorRegistry registry) {
-    registry.addInterceptor(jwtTokenInterceptor)
-        .addPathPatterns("/**")
-        .excludePathPatterns("/","/api/v1/oauth/**",
-            "/v2/**", // swagger
-            "/webjars/**", // swagger
-            "/swagger**", // swagger
-            "/swagger-resources/**");
-  }
+    private final JwtTokenInterceptor jwtTokenInterceptor;
 
-  @Override
-  public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/**")
-        .allowedOrigins("*")
-        .allowedMethods("*")
-        .allowedHeaders("*")
-        .allowedHeaders("authorization")
-        .allowCredentials(false)
-        .maxAge(3000);
-  }
-
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(jwtTokenInterceptor)
+            .addPathPatterns("/**")
+            .excludePathPatterns("/", "/api/v1/oauth/**",
+                "/v2/**", // swagger
+                "/webjars/**", // swagger
+                "/swagger**", // swagger
+                "/swagger-resources/**");
+    }
 }
