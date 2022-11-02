1971dcd30b21ba7bfea31c8e328f3993c0602ba0
bamdule <bamdule5@gmail.com>
2022-11-02 23:00:09 +0900

    refactor : S3ObjectKeyCreatorTest 테스트 코드를 수정한다

--- src/test/java/com/linkgem/infrastructure/common/aws/S3ObjectKeyCreatorTest.java
+++ src/test/java/com/linkgem/infrastructure/common/aws/S3ObjectKeyCreatorTest.java
@@ -9,7 +9,7 @@ class S3ObjectKeyCreatorTest {
 
     @Test
     void createS3ObjectKey() {
-        S3ObjectKeyCreator s3ObjectKeyCreator = new S3ObjectKeyCreator("image");
+        S3ObjectKeyCreator s3ObjectKeyCreator = new S3ObjectKeyCreator("image", "s3domain");
         String url = s3ObjectKeyCreator.create(Directory.LINK, 1L, 1L);
 
         Assertions.assertNotNull(url);
