d5402fc43a1d052d7ea6226dad4c892dabeaae49
bamdule <bamdule5@gmail.com>
2022-11-16 23:27:01 +0900

    feat : CommonLinkRequest, Response DTO를 생성한다

---
+++ src/main/java/com/linkgem/presentation/commonlink/dto/CommonLinkResponse.java
@@ -0,0 +1,52 @@
+package com.linkgem.presentation.commonlink.dto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.linkgem.domain.commonlink.CommonLinkInfo;
+
+import io.swagger.annotations.ApiModel;
+import lombok.Getter;
+
+public class CommonLinkResponse {
+    private CommonLinkResponse() {
+    }
+
+    @ApiModel(description = "잼크루 픽 목록 조회 응답")
+    @Getter
+    public static class Main {
+
+        private Long id;
+        private String url;
+
+        private String title;
+        private String description;
+        private String imageUrl;
+        private String siteName;
+        private LocalDateTime createDate;
+        private LocalDateTime updateDate;
+
+        private Main(CommonLinkInfo.Main commonLinkInfo) {
+            this.id = commonLinkInfo.getId();
+            this.url = commonLinkInfo.getUrl();
+            this.title = commonLinkInfo.getOpenGraphInfo().getTitle();
+            this.description = commonLinkInfo.getOpenGraphInfo().getDescription();
+            this.imageUrl = commonLinkInfo.getOpenGraphInfo().getImageUrl();
+            this.siteName = commonLinkInfo.getOpenGraphInfo().getSiteName();
+            this.createDate = commonLinkInfo.getCreateDate();
+            this.updateDate = commonLinkInfo.getUpdateDate();
+        }
+
+        public static Main of(CommonLinkInfo.Main commonLinkInfo) {
+            return new Main(commonLinkInfo);
+        }
+
+        public static List<Main> ofs(List<CommonLinkInfo.Main> commonLinkInfos) {
+            return commonLinkInfos.stream()
+                .map(Main::of)
+                .collect(Collectors.toList());
+        }
+    }
+
+}
