5b3fefc547e82969c6da8fc60e4f50ac35fe18fc
bamdule <bamdule5@gmail.com>
2023-02-19 19:55:46 +0900

    refactor : eb 배포를 위한 gitactions 설정 수정 (5)

--- src/test/resources/sql/user.sql
+++ src/test/resources/sql/user.sql
@@ -1,4 +1,4 @@
 INSERT INTO user
 (id, nickname, create_date, update_date, oauth_id, career_year, job, login_email, mail_email, name, user_phase,
- profile_image_url)
-VALUES (1, '테스터', now(), now(), '', 1, '개발자', '', '', '', 'REGISTERED', '');
+ profile_image_url, IS_SAVED_FIRST_LINK)
+VALUES (1, '테스터', now(), now(), '', 1, '개발자', '', '', '', 'REGISTERED', '', false);
