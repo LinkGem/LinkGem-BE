f4d4ab145f3aac7a99890df3c247f2f97209c75f
bamdule <bamdule5@gmail.com>
2023-02-22 20:56:11 +0900

    feat : 최초 가입 시 이벤트 알림이 전송된다.

--- src/main/java/com/linkgem/domain/notification/service/create/factory/NotificationCreateFactory.java
+++ src/main/java/com/linkgem/domain/notification/service/create/factory/NotificationCreateFactory.java
@@ -2,6 +2,7 @@ package com.linkgem.domain.notification.service.create.factory;
 
 import org.springframework.stereotype.Component;
 
+import com.linkgem.domain.notification.NotificationButtonAction;
 import com.linkgem.domain.notification.NotificationCommand;
 import com.linkgem.domain.notification.NotificationType;
 import com.linkgem.domain.user.User;
@@ -13,6 +14,17 @@ public class NotificationCreateFactory {
         return NotificationCommand.Create.builder()
             .type(NotificationType.MESSAGE)
             .content(String.format(NotificationMessage.JOIN.getMessage(), user.getNickname()))
+            .receiverId(user.getId())
+            .build();
+    }
+
+    public NotificationCommand.Create createEventNotification(User user) {
+        return NotificationCommand.Create.builder()
+            .type(NotificationType.EVENT)
+            .buttonText("이벤트 참여하기")
+            .buttonAction(NotificationButtonAction.MOVE_LINK)
+            .buttonValue("https://forms.gle/rLTXHQTM46gsgZFg8")
+            .content(NotificationMessage.EVENT20230227.getMessage())
             .receiverId(user.getId())
             .build();
     }
--- src/main/java/com/linkgem/domain/notification/service/create/factory/NotificationMessage.java
+++ src/main/java/com/linkgem/domain/notification/service/create/factory/NotificationMessage.java
@@ -7,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 @Getter
 public enum NotificationMessage {
 
-    JOIN("<mark>%s</mark> 키퍼님 반가워요✨<br/>링크잼에서 보석 같은 나만의 링크들을 쉽게 관리해 보세요.");
+    JOIN("<mark>%s</mark> 키퍼님 반가워요✨<br/>링크잼에서 보석 같은 나만의 링크들을 쉽게 관리해 보세요."),
+
+    EVENT20230227("링크잼이 더 나은 서비스를 제공할 수 있도록 피드백을 남겨주세요! 마음가득 선물을 드립니다.\uD83E\uDDE1");
     private final String message;
 }
